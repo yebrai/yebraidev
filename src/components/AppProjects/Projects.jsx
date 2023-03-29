7679e835a2fbbe6dfb0ce6ac93b74504cda2a434
yebrai <ivansevv@gmail.com>
2023-03-29 18:29:21 +0200

    deleting comments

--- src/components/AppProjects/Projects.jsx
+++ src/components/AppProjects/Projects.jsx
@@ -67,50 +67,6 @@ export default function Projects() {
         )
     })}
       </ul>
-    
-
-      {/* <ul className={styles.mainUl}>
-        <li>
-          <Image></Image>
-          <h4>PDF-SIGNER</h4>
-          <p>pdf signer</p>
-        </li>
-        <li>
-          <Image></Image>
-          <h4>TM Bulletin</h4>
-          <p>pdf signer</p>
-        </li>
-        <li>
-          <Image></Image>
-          <h4>Retroscript</h4>
-          <p>pdf signer</p>
-        </li>
-        <li>
-          <Image></Image>
-          <h4>Taste-it</h4>
-          <p>pdf signer</p>
-        </li>
-        <li>
-          <Image></Image>
-          <h4>Our Home</h4>
-          <p>pdf signer</p>
-        </li>
-        <li>
-          <Image></Image>
-          <h4>Infecnation</h4>
-          <p>pdf signer</p>
-        </li>
-        <li>
-          <Image></Image>
-          <h4>Flappy Bird.ts</h4>
-          <p>pdf signer</p>
-        </li>
-        <li>
-          <Image></Image>
-          <h4>Alleawey</h4>
-          <p>pdf signer</p>
-        </li>
-      </ul> */}
     </>
   );
 }
