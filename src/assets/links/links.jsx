6b315ab2c75b545908b7ab72c7cb00910fafc0a1
yebrai <ivansevv@gmail.com>
2023-05-11 11:16:09 +0200

    cv updated

--- src/assets/links/links.jsx
+++ src/assets/links/links.jsx
@@ -94,7 +94,7 @@ export const portfolioLinks = [
     },
     {
       label: <ImProfile />,
-      route: "https://drive.google.com/file/d/1r3NS_zxG2FsqmFpNMJtkvh0gtgsfx1of/view?usp=share_link",
+      route: "https://drive.google.com/file/d/1K4K1C9jvJ2E0jnPilNqqqJdgCIDxaQJG/view?usp=share_link",
       description: "My Resume",
     },
   ];
