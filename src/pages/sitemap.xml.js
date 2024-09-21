0ae775aca76208a74f71d3109f88137b75d5a7a3
yebrai <ivansevv@gmail.com>
2024-09-21 11:52:03 +0200

    v1.2 Sitemap fix

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -9,7 +9,7 @@ export default function Sitemap() {
   export async function getServerSideProps({ res }) {
     res.setHeader('Content-Type', 'text/xml');
     res.write(`<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
           <loc>https://www.yebraidev.es/</loc>
           <lastmod>${new Date().toISOString()}</lastmod>
