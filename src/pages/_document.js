9f107320369c4060563ea1dce2f3405f7fc28f04
yebrai <ivansevv@gmail.com>
2024-09-21 10:50:12 +0200

    v1.1 (Update gtag)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,14 +7,14 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* Google Tag (gtag.js) */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MENM4EGWC1"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z2J9V8JR3M"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-MENM4EGWC1');
+                gtag('config', 'G-Z2J9V8JR3M');
               `,
             }}
           />
