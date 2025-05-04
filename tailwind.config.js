78fbe9037c72abc7ad2a606c230740ede94da1b6
yebrai <ivansevv@gmail.com>
2025-05-04 22:33:05 +0200

    fixed tailwind but broken styles - Wip fix tailwind or continue module.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",  // Asegúrate de que cubra todos los archivos que usan clases de Tailwind
+    "./pages/**/*.{js,ts,jsx,tsx}", // Incluye las páginas si las tienes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
